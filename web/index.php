6fa520d151e630deecc60da26285bfd33bfd8c83
leocavalcante <lc@leocavalcante.com>
2016-12-05 23:56:14 -0200

    Update to new Siler API

--- web/index.php
+++ web/index.php
@@ -2,13 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-define('ENV_PATH', __DIR__.'/..');
-require_once __DIR__.'/../vendor/leocavalcante/siler/siler.php';
-
-// optional
-define('TWIG_TEMPLATES', __DIR__.'/../templates');
-define('TWIG_TEMPLATES_CACHE', __DIR__.'/../templates/cache');
-require_once __DIR__.'/../vendor/leocavalcante/siler/twig.php';
+load_dotenv(__DIR__.'/..');
+create_twig_env(__DIR__.'/../templates', __DIR__.'/../templates/cache', true);
 
 static_path('/', require_fn(__DIR__.'/pages/home.php'));
 regexp_path('/^\/blog\/(?P<slug>[A-z0-9\-]+)\/?$/', require_fn(__DIR__.'/pages/blog/post.php'));
