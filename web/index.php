dafc2700bc08196cfeb73d0d2aeb8aaae810c42c
leocavalcante <lc@leocavalcante.com>
2016-12-07 18:28:36 -0200

    Add Closure route example

--- web/index.php
+++ web/index.php
@@ -6,5 +6,11 @@ Siler\Dotenv\init(__DIR__.'/..');
 Siler\Twig\init(__DIR__.'/../templates', __DIR__.'/../templates/cache', true);
 
 Siler\route('/^\/$/', Siler\require_fn(__DIR__.'/pages/home.php'));
+
+Siler\route('/^\/fn$/', function () {
+    echo 'Im closure';
+    exit;
+});
+
 Siler\route('/^\/blog\/(?P<slug>[A-z0-9\-]+)\/?$/', Siler\require_fn(__DIR__.'/pages/blog/post.php'));
 Siler\route('/^/', Siler\require_fn(__DIR__.'/pages/404.php'));
