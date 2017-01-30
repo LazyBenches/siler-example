20336a2d8627ed690f5318ee89ddf6557fc35db1
Leo Cavalcante <lc@leocavalcante.com>
2017-01-30 14:44:16 -0200

    Remove unsued blog routing

--- web/index.php
+++ web/index.php
@@ -5,7 +5,6 @@ require_once __DIR__.'/../app/bootstrap.php';
 use function Siler\Route\route;
 
 route('get', '/', 'pages/home.php');
-route('get', '/blog/{slug}', 'pages/blog/post.php');
 
 route('get', '/fn', function () {
     echo 'Im a closure, babe!';
